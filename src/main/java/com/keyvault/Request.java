dc31dc0d2ca9b945a4499189573ac7c66ebd885a
SagAraque <sagaraque@gmail.com>
2023-06-04 12:12:53 +0200

    Request refactor

--- src/main/java/com/keyvault/Request.java
+++ src/main/java/com/keyvault/Request.java
@@ -10,19 +10,6 @@ import java.io.Serializable;
 public class Request implements Serializable {
     @Serial
     private static final long serialVersionUID = 6529685098267757621L;
-    public static String GET = "GET";
-    public static String GET_DEVICES = "GET-DEVICES";
-
-    public static String DELETE = "DELETE";
-    public static String DELETE_USER = "DELETE-USER";
-    public static String INSERT = "INSERT";
-    public static String VERIFY = "VERIFY";
-    public static String MOD = "MOD";
-    public static String LOGIN = "LOGIN";
-    public static String REGISTER = "REGISTER";
-    public static String CLEAR_DEVICE = "CLEAR-DEVICE";
-    public static String TOTP = "TOTP";
-    public static String VERIFY_TOTP = "VERIFY-TOTP";
     private Users user = null;
     private Devices device = null;
     private String operationCode = null;
